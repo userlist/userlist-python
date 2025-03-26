182705f65ae2e08208287770e58885cdbe5cd2c0
Benedikt Deicke <benedikt@benediktdeicke.com>
2025-03-26 14:34:13 +0100

    Adds Userlist alias for the Push class

--- src/userlist/__init__.py
+++ src/userlist/__init__.py
@@ -2,5 +2,7 @@ from .push import Push
 from .config import Config
 from .client import Client
 
+Userlist = Push
+
 __version__ = "0.0.0"
-__all__ = ["Push", "Config", "Client"]
+__all__ = ["Userlist", "Push", "Config", "Client"]
